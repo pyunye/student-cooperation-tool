905b3fdb09c0fd2882dfcf0d9fe80b8b31b3546a
laipuni <2tgb02023@gmail.com>
2024-09-24 14:45:02 +0900

    init commit

---
+++ src/main/java/com/studentcooperationtool/studentcooperationtool/StudentCooperationToolApplication.java
@@ -0,0 +1,13 @@
+package com.studentcooperationtool.studentcooperationtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentCooperationToolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentCooperationToolApplication.class, args);
+    }
+    // 간단한 api 통신을 테스트할 예쩡
+}
---
+++ src/test/java/com/studentcooperationtool/studentcooperationtool/StudentCooperationToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studentcooperationtool.studentcooperationtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentCooperationToolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
